53adb18c559d2243ee34fefe6a8bfe1e14960dee
Yummmmy22 <dimas.japan.drummer@gmail.com>
2024-01-19 22:28:02 +0700

    animation player

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -9,7 +9,7 @@ public class PlayerController : MonoBehaviour
     public CharacterController controller;
     public float gravity = -9.8f;
     private Vector3 velocity;
-    private bool isCharacterMoving;
+    public Animator animator;
     public float playerSPEED = 10f;
     private int jumpCount = 0;
     public float playerJUMPFORCE = 6.5f;
@@ -26,9 +26,11 @@ public class PlayerController : MonoBehaviour
         //Gravity
         if (!controller.isGrounded) { 
             velocity.y += gravity * Time.deltaTime;
+            animator.SetBool("isJumping", true);
         }else
         {
             jumpCount = 0;
+            animator.SetBool("isJumping", false);
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && jumpCount < 2)
@@ -40,14 +42,16 @@ public class PlayerController : MonoBehaviour
         if (direction.magnitude > 0.01f)
         {
             Vector3 skinRotation = skinTransform.eulerAngles;
-            skinRotation.y = Mathf.LerpAngle(skinRotation.y, Mathf.Atan2(-direction.x, -direction.z) * Mathf.Rad2Deg, playerSPEED * 1.5f * Time.deltaTime);
+            skinRotation.y = Mathf.LerpAngle(skinRotation.y, Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg, playerSPEED * 1.5f * Time.deltaTime);
             skinTransform.eulerAngles = skinRotation;
             velocity.x = direction.x * playerSPEED;
             velocity.z = direction.z * playerSPEED;
+            animator.SetBool("isMoving", true);
         }else
         {
             velocity.x = Mathf.MoveTowards(velocity.x, 0, playerSPEED);
             velocity.z = Mathf.MoveTowards(velocity.z, 0, playerSPEED);
+            animator.SetBool("isMoving", false);
         }
 
         controller.Move(velocity * Time.deltaTime);
