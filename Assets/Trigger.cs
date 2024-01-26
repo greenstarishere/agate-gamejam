bf567b77fb5d18940a3b88f04bcaa5d9c607732e
Yummmmy22 <dimas.japan.drummer@gmail.com>
2024-01-27 04:38:14 +0700

    unity event

--- Assets/Trigger.cs
+++ Assets/Trigger.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DialogueEditor;
+using UnityEngine.Events;
 
 public class Trigger : MonoBehaviour
 {
     [SerializeField] private NPCConversation myConversation;
     private bool conversationStarted = false;
+    public UnityEvent onConversationStart;
+    public UnityEvent onConversationEnd;
+
 
     // Start is called before the first frame update
     void Update()
@@ -14,7 +18,16 @@ public class Trigger : MonoBehaviour
         if (Input.anyKey && !conversationStarted)
         {
             conversationStarted = true;
+            onConversationStart.Invoke();
             ConversationManager.Instance.StartConversation(myConversation);
+            ConversationManager.OnConversationEnded -= OnConversationEnded;
+            ConversationManager.OnConversationEnded += OnConversationEnded;
         }
+    }
+
+    private void OnConversationEnded()
+    {
+        onConversationEnd.Invoke();
+        ConversationManager.OnConversationEnded -= OnConversationEnded;
     }
 }
